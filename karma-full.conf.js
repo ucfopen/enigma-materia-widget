23e40db93b89e31c29f9a1d0c1059f5efc2d5059
Brandon Stull <bstull@ucf.edu>
2016-10-25 15:09:59 -0400

    Initial setup, widget is now self-buildable and potentially testable.

---
+++ karma-full.conf.js
@@ -0,0 +1,80 @@
+module.exports = function(config) {
+	config.set({
+
+		autoWatch: true,
+
+		basePath: './',
+
+		browsers: ['PhantomJS'],
+
+		files: [
+			'../../js/*.js',
+			'node_modules/angular/angular.js',
+			'node_modules/angular-mocks/angular-mocks.js',
+			'build/demo.json',
+			'build/modules/*.js',
+			'build/controllers/*.js',
+			'tests/*.js'
+		],
+
+		frameworks: ['jasmine'],
+
+		plugins: [
+			'karma-coverage',
+			'karma-eslint',
+			'karma-jasmine',
+			'karma-json-fixtures-preprocessor',
+			'karma-mocha-reporter',
+			'karma-phantomjs-launcher'
+		],
+
+		preprocessors: {
+			'build/controllers/*.js': ['coverage', 'eslint'],
+			'build/demo.json': ['json_fixtures']
+		},
+
+		//plugin-specific configurations
+		eslint: {
+			stopOnError: true,
+			stopOnWarning: false,
+			showWarnings: true,
+			engine: {
+				configFile: '.eslintrc.json'
+			}
+		},
+
+		jsonFixturesPreprocessor: {
+			variableName: '__demo__'
+		},
+
+		reporters: ['coverage', 'mocha'],
+
+		//reporter-specific configurations
+
+		coverageReporter: {
+			check: {
+				global: {
+					statements: 100,
+					branches:   80,
+					functions:  90,
+					lines:      90
+				},
+				each: {
+					statements: 100,
+					branches:   80,
+					functions:  90,
+					lines:      90
+				}
+			},
+			reporters: [
+				{ type: 'html', subdir: 'report-html' },
+				{ type: 'cobertura', subdir: '.', file: 'coverage.xml' }
+			]
+		},
+
+		mochaReporter: {
+			output: 'autowatch'
+		}
+
+	});
+};
