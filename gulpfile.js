fa79e66e3250d28627168c4cc50f340db512ecaf
William Funk <wfunk@knights.ucf.edu>
2016-12-07 09:32:37 -0500

    Fixed a glitch preventing GUI from installing to Materia.

--- gulpfile.js
+++ gulpfile.js
@@ -21,9 +21,13 @@ var uglify = require('gulp-uglify');
 var zip = require('gulp-zip');
 
 var configs = null;
-if (fs.existsSync('../../backend/config.json'))
+try
 {
 	configs = require('../../backend/config.json');
+}
+catch(e)
+{
+	console.log(e);
 }
 
 var widget = sanitize("enigma");
