8ac3974be1bac8ac0437ad7bdff346d1c3d541ae
Brandon Stull <bstull@ucf.edu>
2018-10-24 12:57:08 -0400

    Removed scrollTo directive usage for now, modified webpack config to solve minification-related bugs in production builds.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,63 @@
 const path = require('path')
+const srcPath = path.join(__dirname, 'src')
+const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
 
-// load the reusable legacy webpack config from materia-widget-dev
-let webpackConfig = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig()
+const rules = widgetWebpack.getDefaultRules()
+const entries = widgetWebpack.getDefaultEntries()
 
 // cusomize the config
-delete webpackConfig.entry['creator.js']
-delete webpackConfig.entry['player.js']
+delete entries['creator.js']
+delete entries['player.js']
 
-webpackConfig.entry['directives/enter.js'] = [path.join(__dirname, 'src', 'directives', 'enter.coffee')]
-webpackConfig.entry['directives/focus.js'] = [path.join(__dirname, 'src', 'directives', 'focus.coffee')]
-webpackConfig.entry['directives/scroll.js'] = [path.join(__dirname, 'src', 'directives', 'scroll.coffee')]
+entries['directives/enter.js'] = [path.join(srcPath, 'directives', 'enter.coffee')]
+entries['directives/focus.js'] = [path.join(srcPath, 'directives', 'focus.coffee')]
+entries['directives/scroll.js'] = [path.join(srcPath, 'directives', 'scroll.coffee')]
 
-webpackConfig.entry['modules/creator.js'] = [path.join(__dirname, 'src', 'modules', 'creator.coffee')]
-webpackConfig.entry['modules/player.js'] = [path.join(__dirname, 'src', 'modules', 'player.coffee')]
+entries['modules/creator.js'] = [path.join(srcPath, 'modules', 'creator.coffee')]
+entries['modules/player.js'] = [path.join(srcPath, 'modules', 'player.coffee')]
 
-webpackConfig.entry['controllers/creator.js'] = [path.join(__dirname, 'src', 'controllers', 'creator.coffee')]
-webpackConfig.entry['controllers/player.js'] = [path.join(__dirname, 'src', 'controllers', 'player.coffee')]
+entries['controllers/creator.js'] = [path.join(srcPath, 'controllers', 'creator.coffee')]
+entries['controllers/player.js'] = [path.join(srcPath, 'controllers', 'player.coffee')]
 
-module.exports = webpackConfig
+let customHTMLAndReplaceRule = {
+	test: /\.html$/i,
+	exclude: /node_modules/,
+	use: [
+		{
+			loader: 'file-loader',
+			options: { name: '[name].html' }
+		},
+		{
+			loader: 'extract-loader'
+		},
+		{
+			loader: 'string-replace-loader',
+			options: { multiple: widgetWebpack.materiaJSReplacements }
+		},
+		{
+			loader: 'html-loader',
+			options: {
+				minifyCSS: false
+			}
+		}
+	]
+}
+
+let customRules = [
+	rules.loaderDoNothingToJs,
+	rules.loaderCompileCoffee,
+	rules.copyImages,
+	customHTMLAndReplaceRule, // <--- replaces "rules.loadHTMLAndReplaceMateriaScripts"
+	rules.loadAndPrefixCSS,
+	rules.loadAndPrefixSASS,
+]
+
+// options for the build
+let options = {
+	entries: entries,
+	moduleRules: customRules
+}
+
+const ourFinalWebpackConfig = widgetWebpack.getLegacyWidgetBuildConfig(options)
+
+module.exports = ourFinalWebpackConfig
