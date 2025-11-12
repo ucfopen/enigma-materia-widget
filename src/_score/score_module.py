7ed5f3fcaec13866ed29275510fe1e95f18782cb
Corey Peterson <corey.peterson@ucf.edu>
2025-11-12 11:01:16 -0500

    Disabled score screen override in install yaml. Cleaned up score module.

--- src/_score/score_module.py
+++ src/_score/score_module.py
@@ -1,14 +1,7 @@
 from scoring.module import ScoreModule
 
 
-
 class EnigmaGS(ScoreModule):
-    def __init__(self,  play):
-        super().__init__(play)
-        opts = self.qset.get("options", {}) if isinstance(self.qset, dict) else {}
-        # this should be handeld by frontend
-        # self.hide_correct = opts.get("hide_correct", True)
-
 
     def check_answer(self, log):
         question = self.get_question_by_item_id(log.item_id)
